93534b1d132300add7748e0c2b2364e6756a529c
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2026-01-21 13:59:05 -0300

    Subindo projeto no repositorio

---
+++ src/test/java/com/git/Nog022/Nolly/NollyApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.git.Nog022.Nolly;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NollyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
